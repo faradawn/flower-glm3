6126a2d9d955f5e1b1fd611eff3b6ef348378e0d
faradawn <faradawny@gmail.com>
2023-12-19 03:29:29 +0000

    done colab finetune, add mom data

--- finetune_chatmodel_demo/scripts/finetune_pt_multiturn.sh
+++ finetune_chatmodel_demo/scripts/finetune_pt_multiturn.sh
@@ -8,14 +8,14 @@ NUM_GPUS=1
 MAX_SEQ_LEN=2048
 DEV_BATCH_SIZE=1
 GRAD_ACCUMULARION_STEPS=16
-MAX_STEP=1000
-SAVE_INTERVAL=500
+MAX_STEP=50
+SAVE_INTERVAL=10
 
 DATESTR=`date +%Y%m%d-%H%M%S`
 RUN_NAME=tool_alpaca_pt
 
 BASE_MODEL_PATH=THUDM/chatglm3-6b
-DATASET_PATH=formatted_data/tool_alpaca.jsonl
+DATASET_PATH=formatted_data/mom_data_v1.jsonl # default tool_alpaca
 OUTPUT_DIR=output/${RUN_NAME}-${DATESTR}-${PRE_SEQ_LEN}-${LR}
 
 mkdir -p $OUTPUT_DIR
@@ -30,7 +30,9 @@ torchrun --standalone --nnodes=1 --nproc_per_node=$NUM_GPUS finetune.py \
     --per_device_train_batch_size $DEV_BATCH_SIZE \
     --gradient_accumulation_steps $GRAD_ACCUMULARION_STEPS \
     --max_steps $MAX_STEP \
-    --logging_steps 1 \
+    --logging_steps 10 \
     --save_steps $SAVE_INTERVAL \
     --learning_rate $LR \
     --pre_seq_len $PRE_SEQ_LEN 2>&1 | tee ${OUTPUT_DIR}/train.log
+
+
